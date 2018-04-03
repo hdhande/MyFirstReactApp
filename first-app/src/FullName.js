e8b34e90c278c4468d3a8d2a92bad290d5c2c43e
dhimali <himali.dhande@opcito.com>
2018-04-03 14:20:22 +0530

    Added FullName component

---
+++ first-app/src/FullName.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FullName extends Component {
+  render() {
+    return (
+      <div>
+        <h4> Himali Dhande</h4>     
+      </div>
+    );
+  }
+}
+
+export default FullName;
